9a5a76d031296b3073a7fc19e5d341686eda4fa7
DigitalDominance <digitaldominanceseo@gmail.com>
2025-01-13 21:37:28 -0500

    Update wasm_rpc.js

--- wasm_rpc.js
+++ wasm_rpc.js
@@ -13,52 +13,55 @@ const {
 
 kaspa.initConsolePanicHook();
 
-(async () => {
-    try {
-        // Initialize RPC client
-        const rpc = new RpcClient({
-            resolver: new Resolver(),
-            networkId: "mainnet",
-        });
+// Initialize RPC client
+const rpc = new RpcClient({
+    resolver: new Resolver(),
+    networkId: "mainnet",
+});
 
-        // Connect to the Kaspa network
-        await rpc.connect();
+console.log("Connecting to RPC...");
+rpc.connect()
+    .then(() => {
         console.log("Connected to RPC:", rpc.url);
 
-        // Generate a new wallet
-        const mnemonic = Mnemonic.random();
-        console.log("Generated mnemonic:", mnemonic.toString());
+        // Generate wallet after successful connection
+        const generateWallet = () => {
+            const mnemonic = Mnemonic.random();
+            console.log("Generated mnemonic:", mnemonic.toString());
 
-        const seed = mnemonic.toSeed();
-        const xPrv = new XPrv(seed);
+            const seed = mnemonic.toSeed();
+            const xPrv = new XPrv(seed);
 
-        // Derive wallet addresses and keys
-        const receiveWalletXPub = xPrv.derivePath("m/44'/111111'/0'/0").toXPub();
-        const receiveAddress = receiveWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
-        console.log("Main Receive Address:", receiveAddress.toString());
+            // Derive wallet addresses and keys
+            const receiveWalletXPub = xPrv.derivePath("m/44'/111111'/0'/0").toXPub();
+            const receiveAddress = receiveWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
 
-        const changeWalletXPub = xPrv.derivePath("m/44'/111111'/0'/1").toXPub();
-        const changeAddress = changeWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
-        console.log("Change Address:", changeAddress.toString());
+            const changeWalletXPub = xPrv.derivePath("m/44'/111111'/0'/1").toXPub();
+            const changeAddress = changeWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
 
-        const privateKey = xPrv.derivePath("m/44'/111111'/0'/0/0").toPrivateKey();
-        console.log("Private Key for Main Receive Address:", privateKey.toString());
+            const privateKey = xPrv.derivePath("m/44'/111111'/0'/0/0").toPrivateKey();
 
-        // Disconnect from RPC
-        await rpc.disconnect();
-        console.log("Disconnected from RPC:", rpc.url);
+            // Wallet details
+            const walletDetails = {
+                mnemonic: mnemonic.toString(),
+                mainReceiveAddress: receiveAddress.toString(),
+                changeAddress: changeAddress.toString(),
+                privateKey: privateKey.toString(),
+            };
 
-        // Print wallet details
-        const walletDetails = {
-            mnemonic: mnemonic.toString(),
-            mainReceiveAddress: receiveAddress.toString(),
-            changeAddress: changeAddress.toString(),
-            privateKey: privateKey.toString(),
+            console.log("Wallet Details:", JSON.stringify(walletDetails, null, 2));
         };
 
-        console.log("Wallet Details:", JSON.stringify(walletDetails, null, 2));
-    } catch (error) {
+        // Generate wallet
+        generateWallet();
+
+        // Disconnect from RPC
+        return rpc.disconnect();
+    })
+    .then(() => {
+        console.log("Disconnected from RPC");
+    })
+    .catch((error) => {
         console.error("Error:", error.message);
         process.exit(1);
-    }
-})();
+    });
