83aff681caa38f285f7f15c5d74e4935150a1faf
DigitalDominance <digitaldominanceseo@gmail.com>
2025-01-13 22:19:56 -0500

    Update wasm_rpc.js

--- wasm_rpc.js
+++ wasm_rpc.js
@@ -1,6 +1,7 @@
 // @ts-ignore
 globalThis.WebSocket = require("websocket").w3cwebsocket; // W3C WebSocket shim
 
+const readline = require("readline");
 const kaspa = require("./wasm/kaspa");
 const {
   Mnemonic,
@@ -23,33 +24,41 @@ rpc.connect().then(() => {
   console.log("Connected to Kaspa RPC");
 });
 
-function createWallet() {
-  const mnemonic = Mnemonic.random();
-  const seed = mnemonic.toSeed();
-  const xPrv = new XPrv(seed);
-  const receiveWalletXPub = xPrv.derivePath("m/44'/111111'/0'/0").toXPub();
-  const receiveAddress = receiveWalletXPub
-    .deriveChild(0, false)
-    .toPublicKey()
-    .toAddress(NetworkType.Mainnet);
-  const changeWalletXPub = xPrv.derivePath("m/44'/111111'/0'/1").toXPub();
-  const changeAddress = changeWalletXPub
-    .deriveChild(0, false)
-    .toPublicKey()
-    .toAddress(NetworkType.Mainnet);
-  const privateKey = xPrv.derivePath("m/44'/111111'/0'/0/0").toPrivateKey();
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+});
 
-  return {
-    mnemonic: mnemonic.toString(),
-    mainReceiveAddress: receiveAddress.toString(),
-    changeAddress: changeAddress.toString(),
-    privateKey: privateKey.toString(),
-  };
+function createWallet() {
+    const mnemonic = Mnemonic.random();
+    const seed = mnemonic.toSeed();
+    const xPrv = new XPrv(seed);
+
+    const receiveWalletXPub = xPrv.derivePath("m/44'/111111'/0'/0").toXPub();
+    const mainReceiveAddress = receiveWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
+
+    const changeWalletXPub = xPrv.derivePath("m/44'/111111'/0'/1").toXPub();
+    const changeAddress = changeWalletXPub.deriveChild(0, false).toPublicKey().toAddress(NetworkType.Mainnet);
+
+    const privateKey = xPrv.derivePath("m/44'/111111'/0'/0/0").toPrivateKey();
+
+    return {
+        mnemonic: mnemonic.toString(),
+        mainReceiveAddress: mainReceiveAddress.toString(),
+        changeAddress: changeAddress.toString(),
+        privateKey: privateKey.toString(),
+    };
 }
 
-process.on("message", (msg) => {
-  if (msg === "create_wallet") {
-    const wallet = createWallet();
-    process.send(wallet);
-  }
+// Command processing
+rl.on("line", (line) => {
+    const command = line.trim();
+
+    if (command === "create_wallet") {
+        const wallet = createWallet();
+        console.log(JSON.stringify(wallet)); // Send wallet data back to Python
+    } else {
+        console.error(`Unknown command: ${command}`);
+    }
 });
