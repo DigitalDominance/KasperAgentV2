62100ee39304918c1952d78197de2d0e0e9897a8
DigitalDominance <digitaldominanceseo@gmail.com>
2025-01-14 18:26:12 -0500

    Update kasper_telegram_bot.py

--- kasper_telegram_bot.py
+++ kasper_telegram_bot.py
@@ -349,7 +349,8 @@ async def topup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         f"🔍 **Wallet Address:** `{wallet_address}`\n\n"
         f"⚖️ **Conversion Rate:** 200 KASPER = 1 Credit\n\n"
-        "Send KASPER tokens to this address to top up your credits.",
+        "💰 Send KASPER tokens to this address to top up your credits."
+	"👻 Once transaction is finished, click /endtopup to receive credits",
         parse_mode="Markdown"
     )
 
